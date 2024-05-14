3b22f8c80d31455d885c101f54bda1e5ab62c3e5
Olaf Lange <lange_olaf@hotmail.com>
2024-05-14 12:49:51 +0200

    console app working

--- myApp/app.py
+++ myApp/app.py
@@ -1,3 +1,4 @@
+
 todo_list = []
 # todo_list.append("Buy milk")
 # todo_list.append("Learn more about Azure")
@@ -11,31 +12,49 @@ todo_list = []
 # for todo in todo_list:
 #     print(todo)
 
+try:
+    with open("Todo.txt", "r") as file:
+        for line in file:
+            todo_list.append(line.strip())
+except FileNotFoundError:
+    print("No saved items found")
 # continue to loop and display menu until the user selects to exit the program
 while True:
-    print() # add a couple of blank lines
+    print()  # add a couple of blank lines
     print()
-    print("To-do list: ") # print the title of the list
-    for todo in todo_list: # loop through existing to-do items
+    print("To-do list: ")  # print the title of the list
+    for todo in todo_list:  # loop through existing to-do items
         print(todo)
 
     # Print the menu
-    print() # add a blank line
+    print()  # add a couple of blank lines
     print("Actions:")
     print("A - Add to-do item")
+    print("R - Remove to-do item")  # <--- ***HERE***
     print("X - Exit")
-    choice = input("Enter your choice (A or X): ")
-    choice = choice.upper() # converts the choice to uppercase
-
+    # <--- ***ALSO UPDATE MENU OPTIONS with the 'R' ***
+    choice = input("Enter your choice (A, R, or X): ")
+    choice = choice.upper()  # converts the choice to uppercase
     # user selected 'a' or 'A' to add an item to the list
     if choice == "A":
-        todo = input("Enter the to-do item: ") 
+        todo = input("Enter the to-do item: ")
         todo_list.append(todo)
         continue  # tells the program to go back to the start of the loop
-
+    # user selected 'r' or 'R' to remove an item from the list
+    if choice == "R":
+        item_number = int(input("Enter the number of the item to remove: "))
+        if item_number > 0 and item_number <= len(todo_list):
+            todo_list.pop(item_number - 1)
+        else:
+            print("Invalid item number")
+        continue
     # user selected 'x' or 'X' to exit the program
     if choice == "X":
-        break # tells the program to exit the loop
+        # write out todo_list to file Todo on exit
+        with open("Todo.txt", "w") as file:
+            for todo in todo_list:
+                file.write(todo + "\n")
+        break  # tells the program to exit the loop
 
     # user selected something else
     print("Invalid choice")
